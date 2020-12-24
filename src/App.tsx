b2334e96c0be86907d05b3fdc9cc6c8066e0228d
YSRKEN <ysr.ken@gmail.com>
2020-12-24 18:41:40 +0900

    [add] react-bootstrapを導入した

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+  return <Container>
+    <Row className="my-3">
+      <Col className="text-center">
+        <h1>マウントアダプター検索支援</h1>
+      </Col>
+    </Row>
+  </Container>;
 }
 
 export default App;
